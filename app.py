b72d1cfa18d61a0f6c9d05c25acaf136d45e6339
Christofer Bryan <christoferbryan45@gmail.com>
2025-12-12 23:54:30 -0500

    added markdowns to notebook

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def create_model(num_classes=196):
         nn.BatchNorm1d(256),
         nn.ReLU(),
         nn.Dropout(0.2),
-        nn.Linear(256, 196)
+        nn.Linear(256, num_classes)
     )
     
     return model
